7e726c78614c7443331c79d8ed7bf682c8f7e8f2
MrCabbageStick <mrcabbagestick@gmail.com>
2025-02-21 17:41:58 +0100

    Simple graph implementation

---
+++ src/main/java/dev/mrcabbagestick/graph/graph/GraphNode.java
@@ -0,0 +1,3 @@
+package dev.mrcabbagestick.graph.graph;
+
+public record GraphNode<T>(T data){}
---
+++ src/main/java/dev/mrcabbagestick/graph/graph/LinkType.java
@@ -0,0 +1,7 @@
+package dev.mrcabbagestick.graph.graph;
+
+public enum LinkType {
+    TYPE_1,
+    TYPE_2,
+    TYPE_3;
+}
