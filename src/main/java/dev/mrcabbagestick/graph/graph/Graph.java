06e0ebb526043de086c3dc1770ca81b6c2c49bd8
MrCabbageStick <mrcabbagestick@gmail.com>
2025-02-22 21:50:44 +0100

    Add graph link removal

--- src/main/java/dev/mrcabbagestick/graph/graph/Graph.java
+++ src/main/java/dev/mrcabbagestick/graph/graph/Graph.java
@@ -1,22 +1,22 @@
 package dev.mrcabbagestick.graph.graph;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.util.HashSet.newHashSet;
 
 public class Graph<T> {
-    private Map<GraphNode<T>, HashSet<GraphLink<T>>> adjacencyList = new HashMap<>();
+    private final Map<GraphNode<T>, HashSet<GraphLink<T>>> adjacencyList = new HashMap<>();
 
     public Graph(GraphNode<T> firstNode){
         adjacencyList.put(firstNode, newHashSet(0));
     }
 
-    private void addNode_unsafe(GraphNode<T> node){
-        adjacencyList.put(node, newHashSet(0));
-    }
+    private Graph(){}
+
+//    private void addNode_unsafe(GraphNode<T> node){
+//        adjacencyList.put(node, newHashSet(0));
+//    }
 
     public boolean addNode(GraphNode<T> node, GraphNode<T> connectedTo, LinkType linkType){
         var allNodes = adjacencyList.keySet();
@@ -49,6 +49,88 @@ public class Graph<T> {
 
     public Map<GraphNode<T>, Set<GraphLink<T>>> getAdjacencies(){
         return Map.copyOf(adjacencyList);
+    }
+
+    private boolean removeConnection_unsafe(GraphNode<T> node1, GraphNode<T> node2){
+        var allNodes = adjacencyList.keySet();
+
+        if(!allNodes.contains(node1) || !allNodes.contains(node2))
+            return false;
+
+        var node1Connections = adjacencyList.get(node1);
+        var node2Connections = adjacencyList.get(node2);
+
+        adjacencyList.put(
+                node1,
+                node1Connections.stream().filter(link -> link.node() != node2).collect(Collectors.toCollection(HashSet::new))
+        );
+
+        adjacencyList.put(
+                node2,
+                node2Connections.stream().filter(link -> link.node() != node1).collect(Collectors.toCollection(HashSet::new))
+        );
+
+        return true;
+    }
+
+    public Optional<Graph<T>> removeConnectionAndSplit(GraphNode<T> node1, GraphNode<T> node2){
+        boolean nodesDisconnected = removeConnection_unsafe(node1, node2);
+
+        // Nodes not in network
+        if(!nodesDisconnected)
+            return Optional.empty();
+
+        var connectedToNode1 = getConnectedNodes(node1);
+
+        // Nodes still connected after link removal
+        if(connectedToNode1.contains(node2))
+            return Optional.empty();
+
+        Graph<T> newGraph = new Graph<>();
+
+        HashSet<GraphNode<T>> connectedToNode2 = new HashSet<>(adjacencyList.size() - connectedToNode1.size());
+
+        // Populate new graph
+        adjacencyList.forEach((key, value) -> {
+            if (!connectedToNode1.contains(key)) {
+                connectedToNode2.add(key);
+                newGraph.adjacencyList.put(key, value);
+            }
+        });
+
+        // Remove unneeded nodes from this graph
+        connectedToNode2.forEach(adjacencyList::remove);
+
+        return Optional.of(newGraph);
+    }
+
+    public Set<GraphNode<T>> getConnectedNodes(GraphNode<T> node){
+        if(!adjacencyList.containsKey(node))
+            return Set.of();
+
+        Map<GraphNode<T>, Set<GraphNode<T>>> adjacentNodes = new HashMap<>(adjacencyList.size());
+
+        adjacencyList.forEach((key, value) -> {
+            var nodeSet = value.stream().map(GraphLink::node).collect(Collectors.toSet());
+            adjacentNodes.put(key, nodeSet);
+        });
+
+        HashSet<GraphNode<T>> visitedNodes = new HashSet<>();
+        Stack<GraphNode<T>> nodesToVisit = new Stack<>();
+
+        nodesToVisit.push(node);
+
+        while(!nodesToVisit.empty()){
+            var currentNode = nodesToVisit.pop();
+            visitedNodes.add(currentNode);
+
+            adjacentNodes.get(currentNode).forEach(_node -> {
+                if(!visitedNodes.contains(_node))
+                    nodesToVisit.push(_node);
+            });
+        }
+
+        return visitedNodes;
     }
 
     public void printAdjacencies(){
--- src/test/java/dev/mrcabbagestick/graph/graph/GraphTests.java
+++ src/test/java/dev/mrcabbagestick/graph/graph/GraphTests.java
@@ -3,6 +3,7 @@ package dev.mrcabbagestick.graph.graph;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -70,5 +71,95 @@ public class GraphTests {
         );
 
         assertEquals(graph.getAdjacencies(), correctStructure);
+    }
+
+    @Test
+    void graph_ConnectedNodes(){
+        GraphNode<String> nodeA = new GraphNode<>("A");
+        Graph<String> graph = new Graph<>(nodeA);
+
+        GraphNode<String> nodeB = new GraphNode<>("B");
+        GraphNode<String> nodeC = new GraphNode<>("C");
+
+        assertTrue(graph.addNode(nodeB, nodeA, LinkType.TYPE_1));
+        assertTrue(graph.addNode(nodeC, nodeB, LinkType.TYPE_2));
+        assertTrue(graph.addLink(nodeC, nodeA, LinkType.TYPE_3));
+
+        var connectedNodes = graph.getConnectedNodes(nodeA);
+
+        assertEquals(connectedNodes, Set.of(nodeA, nodeB, nodeC));
+    }
+
+    @Test
+    void graphConnectionRemoval_Unsafe_Triangle(){
+        GraphNode<String> nodeA = new GraphNode<>("A");
+        Graph<String> graph = new Graph<>(nodeA);
+
+        GraphNode<String> nodeB = new GraphNode<>("B");
+        GraphNode<String> nodeC = new GraphNode<>("C");
+
+        assertTrue(graph.addNode(nodeB, nodeA, LinkType.TYPE_1));
+        assertTrue(graph.addNode(nodeC, nodeB, LinkType.TYPE_2));
+        assertTrue(graph.addLink(nodeC, nodeA, LinkType.TYPE_3));
+
+        var secondGraph = graph.removeConnectionAndSplit(nodeA, nodeB);
+
+        assertEquals(secondGraph, Optional.empty());
+
+        Map<GraphNode<String>, Set<GraphLink<String>>> correctStructure = Map.of(
+                nodeA, Set.of(new GraphLink<>(nodeC, LinkType.TYPE_3)),
+                nodeB, Set.of(new GraphLink<>(nodeC, LinkType.TYPE_2)),
+                nodeC, Set.of(new GraphLink<>(nodeA, LinkType.TYPE_3), new GraphLink<>(nodeB, LinkType.TYPE_2))
+        );
+
+        assertEquals(graph.getAdjacencies(), correctStructure);
+    }
+
+    @Test
+    void graphConnectionRemoval_Split_TwoNodes(){
+        GraphNode<String> nodeA = new GraphNode<>("A");
+        Graph<String> graph = new Graph<>(nodeA);
+
+        GraphNode<String> nodeB = new GraphNode<>("B");
+
+        assertTrue(graph.addNode(nodeB, nodeA, LinkType.TYPE_1));
+
+        var newGraph = graph.removeConnectionAndSplit(nodeA, nodeB);
+        assertFalse(newGraph.isEmpty());
+
+        Map<GraphNode<String>, Set<GraphLink<String>>> correctStructure1 = Map.of(nodeA, Set.of());
+        assertEquals(graph.getAdjacencies(), correctStructure1);
+
+        Map<GraphNode<String>, Set<GraphLink<String>>> correctStructure2 = Map.of(nodeB, Set.of());
+        assertEquals(newGraph.get().getAdjacencies(), correctStructure2);
+    }
+
+    @Test
+    void graphConnectionRemoval_Split_FourNodes(){
+        GraphNode<String> nodeA = new GraphNode<>("A");
+        Graph<String> graph = new Graph<>(nodeA);
+
+        GraphNode<String> nodeB = new GraphNode<>("B");
+        GraphNode<String> nodeC = new GraphNode<>("C");
+        GraphNode<String> nodeD = new GraphNode<>("D");
+
+        assertTrue(graph.addNode(nodeB, nodeA, LinkType.TYPE_1));
+        assertTrue(graph.addNode(nodeC, nodeB, LinkType.TYPE_1));
+        assertTrue(graph.addNode(nodeD, nodeC, LinkType.TYPE_1));
+
+        var newGraph = graph.removeConnectionAndSplit(nodeB, nodeC);
+        assertFalse(newGraph.isEmpty());
+
+        Map<GraphNode<String>, Set<GraphLink<String>>> correctStructure1 = Map.of(
+                nodeA, Set.of(new GraphLink<>(nodeB, LinkType.TYPE_1)),
+                nodeB, Set.of(new GraphLink<>(nodeA, LinkType.TYPE_1))
+        );
+        assertEquals(graph.getAdjacencies(), correctStructure1);
+
+        Map<GraphNode<String>, Set<GraphLink<String>>> correctStructure2 = Map.of(
+                nodeC, Set.of(new GraphLink<>(nodeD, LinkType.TYPE_1)),
+                nodeD, Set.of(new GraphLink<>(nodeC, LinkType.TYPE_1))
+        );
+        assertEquals(newGraph.get().getAdjacencies(), correctStructure2);
     }
 }
